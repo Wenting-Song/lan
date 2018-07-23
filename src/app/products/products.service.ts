cc87e163b9a087074905c213430cbe7126878a28
songwe <wenting.song@verizon.com>
2018-07-22 22:16:27 -0400

    more products added

--- src/app/products/products.service.ts
+++ src/app/products/products.service.ts
@@ -10,10 +10,18 @@ export class ProductService {
         // tslint:disable-next-line:max-line-length
         'https://res.lancome.com.cn/resources/2018/6/5/15281869292466311_920X920.jpg?version=20180705230743340'),
         // tslint:disable-next-line:max-line-length
-        new Product('Absolue Premium Cream SPF15 兰蔻菁纯卓颜日霜 SPF15(15g)', 350, 'SPF 15',
+        new Product('Absolue Premium Cream SPF15 兰蔻菁纯卓颜日霜 SPF15(15g)', 346, 'SPF 15',
          'https://res.lancome.com.cn/resources/2018/5/14/1526270256341804_258X258.jpg'),
-        new Product('Absolue Premium Cream SPF15 兰蔻菁纯卓颜日霜 SPF15(15g)', 350, 'SPF 15',
-         'https://res.lancome.com.cn/resources/2018/6/5/15281869363711616_258X258.jpg')
+        new Product('Absolue Premium Yeux SPF15 兰蔻菁纯卓颜眼霜 (6g)', 154, '特别设计的眼霜，富含卓颜分子',
+         'https://res.lancome.com.cn/resources/2018/6/5/15281869363711616_258X258.jpg'),
+        new Product('Energie de vie Foam Cleanser SPF15 兰蔻清滢嫩肤洁面乳 (50ml)', 48, '洗面奶',
+         'https://res.lancome.com.cn/resources/2018/6/5/15281869363711616_258X258.jpg'),
+        new Product('兰蔻水光润面霜 (50ml)', 346, '清爽不黏腻，盈润美肌',
+         // tslint:disable-next-line:max-line-length
+         'https://www.lancome-usa.com/dw/image/v2/AANG_PRD/on/demandware.static/-/Sites-lancome-us-master-catalog/default/dwdd2f1150/Products/edv/3614271304698_EnergieDeVie_DayCream50mL_1000x1000.jpg?sw=750&sh=750&sm=fit'),
+         new Product('兰蔻新梦魅旋密睫毛膏 (4ml)', 150, '采用专利睫毛提升刷，使睫毛根部及梢部即刻立体旋密',
+         // tslint:disable-next-line:max-line-length
+         'https://res.lancome.com.cn/resources/2018/5/15/15263560965182208_920X920.jpg?version=20180716205519121'),
 
       ];
 
