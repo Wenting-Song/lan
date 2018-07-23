90ed33a7d06cb8f0d72ae5f0b969446b272e5f58
songwe <wenting.song@verizon.com>
2018-07-22 22:26:20 -0400

    nothing

--- src/app/products/products.service.ts
+++ src/app/products/products.service.ts
@@ -4,8 +4,6 @@ import { EventEmitter } from '@angular/core';
 export class ProductService {
     productSelected = new EventEmitter<Product>();
     private  products: Product[] = [
-        new Product('A cream', 2, 'This is for day time use', // tslint:disable-next-line:max-line-length
-        'https://i03.hsncdn.com/is/image/HomeShoppingNetwork/prodfull/lancome-renergie-lift-multi-action-spf-15-face-cream-d-2016060313511942~203348.jpg'),
         new Product('Absolue Premium Nuit 兰蔻菁纯卓颜晚霜 (15g)', 246, 'This is for night time use',
         // tslint:disable-next-line:max-line-length
         'https://res.lancome.com.cn/resources/2018/6/5/15281869292466311_920X920.jpg?version=20180705230743340'),
